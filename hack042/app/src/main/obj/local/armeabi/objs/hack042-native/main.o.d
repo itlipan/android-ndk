14bf929332c54b114bcb6e7f854d1b70a19a66a4
Paullee <itlipan@gmail.com>
2016-04-27 23:49:22 +0800

    [WN]
    Init Project
    
    First Commit

---
+++ hack042/app/src/main/obj/local/armeabi/objs/hack042-native/main.o.d
@@ -0,0 +1,89 @@
+C:/Files_D/Sites/GoogleNdk/android-ndk/hack042/app/src/main/obj/local/armeabi/objs/hack042-native/main.o: \
+ C:/Files_D/Sites/GoogleNdk/android-ndk/hack042/app/src/main/jni/main.cpp \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/jni.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ C:/Files_D/Sites/GoogleNdk/android-ndk/hack042/app/src/main/jni/sqlite3.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/assert.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/android/log.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/math.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/stdint.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/limits.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ C:/Files_D/Androidsdk/ndk-bundle/sources/cxx-stl/system/include/cstddef \
+ C:/Files_D/Sites/GoogleNdk/android-ndk/hack042/app/src/main/jni/sqlite3.h
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/jni.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+C:/Files_D/Sites/GoogleNdk/android-ndk/hack042/app/src/main/jni/sqlite3.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/assert.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/math.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/limits.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+C:/Files_D/Androidsdk/ndk-bundle/sources/cxx-stl/system/include/cstddef:
+
+C:/Files_D/Sites/GoogleNdk/android-ndk/hack042/app/src/main/jni/sqlite3.h:
